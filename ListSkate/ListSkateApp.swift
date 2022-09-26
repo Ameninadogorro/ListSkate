5f26ef612fcf3b43b60421282912119b9dbf303f
A menina do gorro {Ana Raiany} <101668266+Ameninadogorro@users.noreply.github.com>
2022-09-26 15:37:54 -0300

    Initial Commit

---
+++ ListSkate/ListSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSkateApp.swift
+//  ListSkate
+//
+//  Created by Ana Raiany Guimarães Gomes on 2022-09-26.
+//
+
+import SwiftUI
+
+@main
+struct ListSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
